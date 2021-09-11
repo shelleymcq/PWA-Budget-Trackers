3c719ac43b688c2d286b6ba070e9cccccfa71102
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-09-11 15:31:36 -0400

    deploying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
